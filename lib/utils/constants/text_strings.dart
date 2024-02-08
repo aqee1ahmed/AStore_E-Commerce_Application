0ea805eb64d829b12506729a24c808dd2e9923f6
Aqeel Ahmed <ahmedaqeel1162002@gmail.com>
2024-02-08 16:38:43 +0500

    text string added

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,21 @@
+class AStoreAppTextStrings {
+  AStoreAppTextStrings._();
+
+  ///global text
+
+  ///onboarding text
+  static const String onboardingTitle1 = 'Choose your product';
+  static const String onboardingTitle2 = 'Select payment method';
+  static const String onboardingTitle3 = 'Deliever to your door step';
+
+  static const String onboardingSubtitle1 =
+      'Welcome to a World of Limitless Choices - Your Perfect Product Await!';
+  static const String onboardingSubtitle2 =
+      'For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!';
+  static const String onboardingSubtitle3 =
+      'From Our DoorStep to Yours - Swift, Secure and Contactless Delivery!';
+
+  ///home text
+  static const String homeAppBarTitle = 'Good day for shopping';
+  static const String homeAppBarSubTitle = 'Aqeel Ahmed';
+}
