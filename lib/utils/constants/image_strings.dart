b6650bd7fe9795c296c6b26bdc6f8c651c9d5760
Aqeel Ahmed <ahmedaqeel1162002@gmail.com>
2024-02-08 16:27:19 +0500

    image strings added

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,13 @@
+class AStoreAppImageStrings {
+  AStoreAppImageStrings._();
+
+  ///app logo
+  static const String appLogoDarkMode =
+      'assets/logos/a_store_app_logo_light.png';
+  static const String appLogoLightMode =
+      'assets/logos/a_store_app_logo_dark.png';
+
+  ///social media logo
+  static const String facebookLogo = 'assets/logos/facebook_logo.png';
+  static const String googleLogo = 'assets/logos/google_logo.png';
+}
