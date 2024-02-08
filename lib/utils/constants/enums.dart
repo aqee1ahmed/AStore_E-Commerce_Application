a674a677fcaa1493e89862b57328f01e64ecc8f1
Aqeel Ahmed <ahmedaqeel1162002@gmail.com>
2024-02-08 16:46:28 +0500

    enums added

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,23 @@
+enum TextSizes {
+  small,
+  medium,
+  large,
+}
+
+enum OrderStatus {
+  processing,
+  shipped,
+  delivered,
+}
+
+enum PaymentMethod {
+  paypal,
+  applePay,
+  googlePay,
+  creditCard,
+  masterCard,
+  vise,
+  paystack,
+  paytm,
+  rozarPay,
+}
