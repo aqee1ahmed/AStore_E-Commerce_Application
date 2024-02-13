acefe4350b395392e3cfaad3f46d471eb18476f5
Aqeel Ahmed <ahmedaqeel1162002@gmail.com>
2024-02-13 23:30:00 +0500

    validations added

--- lib/utils/validators/validation.dart
+++ lib/utils/validators/validation.dart
@@ -1,1 +1,55 @@
+class AStoreAppValidators {
+  AStoreAppValidators._();
 
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    //regular expression for email validation
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Password is required';
+    }
+    //check if the password is at least 6 characters long
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+
+    //check uppercase letter
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter';
+    }
+
+    //check for numbers
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number';
+    }
+
+    //check for special characters
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return 'Password must contain at least one special character';
+    }
+    return null;
+  }
+
+  static String? validPhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    }
+    //regular expression for phone number validation(assuming the phone number is 10 digits long)
+    final phoneRegex = RegExp(r'^\d{10}$');
+
+    if (!phoneRegex.hasMatch(value)) {
+      return 'Please enter a valid phone number (10 digits required)';
+    }
+    return null;
+  }
+}
